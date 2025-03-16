bcc9e2964841e66aff8efe8fdacdec6deb7fd879
Divergent AI <iam@divergentoutlier.com>
2025-03-16 18:32:54 +0200

    Move node registration to src/nodes.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,4 @@
 from .clip_token_counter import CLIPTokenCounter
 from .utf8_encoder_node import UTF8EncoderNode
 from .data_store_node import DataStoreNode
-
-NODE_CLASS_MAPPINGS = {
-    "CLIPTokenCounter": CLIPTokenCounter,
-    "UTF8EncoderNode": UTF8EncoderNode,
-    "DataStoreNode": DataStoreNode
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "CLIPTokenCounter": "CLIP Token Counter",
-    "UTF8EncoderNode": "UTF8 Encoder",
-    "DataStoreNode": "Data Store"
-}
+from .src.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
---
+++ src/nodes.py
@@ -0,0 +1,11 @@
+NODE_CLASS_MAPPINGS = {
+    "CLIPTokenCounter": "CLIP Token Counter",
+    "UTF8EncoderNode": "UTF8 Encoder",
+    "DataStoreNode": "Data Store"
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "CLIPTokenCounter": "CLIP Token Counter",
+    "UTF8EncoderNode": "UTF8 Encoder",
+    "DataStoreNode": "Data Store"
+}
