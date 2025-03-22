40c277f26d7bdd4372c5e1e5916048a1075a38b2
Divergent AI <iam@divergentoutlier.com>
2025-03-22 17:40:18 +0200

    Add import for GemmaMultimodalOllama in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .clip_token_counter import CLIPTokenCounter
 from .utf8_encoder_node import UTF8EncoderNode
 from .data_store_node import DataStoreNode
+from .gemma_multimodal_ollama_node import GemmaMultimodalOllama
 
 NODE_CLASS_MAPPINGS = {
     "CLIPTokenCounter": CLIPTokenCounter,
