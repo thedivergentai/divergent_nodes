aada383462e169d05283110274ef633f83d7568f
Divergent AI <iam@divergentoutlier.com>
2025-03-22 19:45:01 +0200

    Rename class to GemmaMultimodal

--- gemma_multimodal_ollama_node.py
+++ gemma_multimodal_ollama_node.py
@@ -19,7 +19,7 @@ except ImportError:
 
 
 
-class GemmaMultimodalOllama:
+class GemmaMultimodal:
     """
     A custom ComfyUI node to integrate the Gemma-3-27b-it-abliterated model for multimodal tasks.
     """
